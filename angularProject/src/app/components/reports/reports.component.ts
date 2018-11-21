5cda3b019d218d2ac134a66a9cfb991c3648b69b
NechamaShraiber <Nechama.Shraiber@seldatinc.com>
2018-11-21 10:35:44 +0200

    r2

--- angularProject/src/app/components/reports/reports.component.ts
+++ angularProject/src/app/components/reports/reports.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit , Injectable} from '@angular/core';
-import { TreeNode } from '../../../../node_modules/@angular/router/src/utils/tree';
-import { Project } from '../../shared/models/project';
 import { ManagerService } from '../../shared/service/manager.service';
-import { NG_PROJECT_AS_ATTR_NAME } from '../../../../node_modules/@angular/core/src/render3/interfaces/projection';
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 import {BehaviorSubject} from 'rxjs';
@@ -104,10 +101,7 @@ this.projectNameAndHours.push({
     this.dataChange.next(data);
   }
 
-  /**
-   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
-   * The return value is the list of `FileNode`.
-   */
+ 
   buildFileTree(obj: {[key: string]: any}, level: number): FileNode[] {
     return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
       const value = obj[key];
@@ -163,141 +157,5 @@ export class ReportsComponent implements OnInit {
 
   
     }
-/*
-
-getProjectInfo(project: TreeTable) {
-    //let projectDays: number = this.baseService.dateDiffInDays(project.startDate, project.endDate);
-    // let date = new Date();
-    // if (date > project.endDate)
-    //   date = project.endDate;
-    // let workedDays: number = this.baseService.dateDiffInDays(project.startDate, date);
-    // let daysPercent: number = workedDays / projectDays;
-
-    // let projectPresenseHours: number = this.projectService.getPresenceHours(project);
-    // let projectPercentHours: number = this.projectService.getPercentHours(project);
-    // let state: string;
-
-    // if (projectPercentHours == daysPercent)
-    //   state = "good";
-    // else
-    //   if (projectPercentHours > daysPercent)
-    //     state = "excellent";
-    //   else
-    //     state = "bad"
-    let hours = project.Project.QaHours + project.Project.UiUxHours + project.Project.DevelopersHours;
-    let actualhorsForProject = this.getActualHoursForProject(project);
-    // console.log("hh", this.teamLeader);
-    let root = {
-      data: {
-        name: "vvv",
-      },
-      children: []
-    };
-    let actualHoursForDepartment = this.getActualHoursForDepartment(project, "DevelopersHours")
-    let departmentNode = {
-      data: {
-        name: "DevelopersHours",
-        hours: project.Project.DevelopersHours,
-        actualHours: actualHoursForDepartment,
-        percent: this.getPrecentOfNumbers(hours, actualHoursForDepartment),
-        // presence: this.baseService.toShortNumber(presenceHoursForDepartment),
-        // percent: departmentHours.numHours > 0 ? this.baseService.toPercent(presenceHoursForDepartment / departmentHours.numHours) : '-'
-      },
-
-      children: [
-
-      ]
-    };
-    project.DetailsWorkerInProjects.forEach(worker => {
-
-
-      if (worker.Kind == "Developers") {
-        let actualHoursforWorker = this.getCountHours(worker)
-        let workerNode = {
-          data: {
-            name: worker.Name,
-            actualHours: actualHoursforWorker,
-            hours: worker.Hours,
-            percent: this.getPrecentOfNumbers(hours, actualHoursforWorker),
-            teamLeader: worker.TeamLeaderName
-            // presence: this.baseService.toShortNumber(presenceHoursForWorker),
-            // percent: worker.workerHours.length ? this.baseService.toPercent(presenceHoursForWorker / worker.workerHours[0].numHours) : '-'
-          }
-        };
-        departmentNode.children.push(workerNode);
-      }
-    })
-    root.children.push(departmentNode);
-    let actualHoursForDepartment1 = this.getActualHoursForDepartment(project, "QaHours");
-    let departmentNode1 = {
-      data: {
-        name: "QaHours",
-        hours: project.Project.QaHours,
-        actualHours: actualHoursForDepartment1,
-        percent: this.getPrecentOfNumbers(hours, actualHoursForDepartment1),
-        // presence: this.baseService.toShortNumber(presenceHoursForDepartment),
-        // percent: departmentHours.numHours > 0 ? this.baseService.toPercent(presenceHoursForDepartment / departmentHours.numHours) : '-'
-      },
-
-      children: [
-
-      ]
-    };
-
-    project.DetailsWorkerInProjects.forEach(worker => {
-      if (worker.Kind == "QA") {
-        let actualHoursforWorker = this.getCountHours(worker)
-        let workerNode = {
-          data: {
-            name: worker.Name,
-            actualHours: actualHoursforWorker,
-            hours: worker.Hours,
-            percent: this.getPrecentOfNumbers(hours, actualHoursforWorker),
-            teamLeader: worker.TeamLeaderName
-            // presence: this.baseService.toShortNumber(presenceHoursForWorker),
-            // percent: worker.workerHours.length ? this.baseService.toPercent(presenceHoursForWorker / worker.workerHours[0].numHours) : '-'
-          }
-        };
-        departmentNode1.children.push(workerNode);
-      }
-    })
-    root.children.push(departmentNode1);
-    let actualHoursForDepartment2 = this.getActualHoursForDepartment(project, "UiUxHours");
-    let departmentNode2 = {
-      data: {
-        name: "UiUxHours",
-        hours: project.Project.UiUxHours,
-        actualHours: actualHoursForDepartment2,
-        percent: this.getPrecentOfNumbers(hours, actualHoursForDepartment2),
-        // presence: this.baseService.toShortNumber(presenceHoursForDepartment),
-        // percent: departmentHours.numHours > 0 ? this.baseService.toPercent(presenceHoursForDepartment / departmentHours.numHours) : '-'
-      },
-      children: [
-
-      ]
-    };
-
-    project.DetailsWorkerInProjects.forEach(worker => {
-      if (worker.Kind == "UI/UX") {
-        let actualHoursforWorker = this.getCountHours(worker)
-        let workerNode = {
-          data: {
-            name: worker.Name,
-            actualHours: actualHoursforWorker,
-            percent: this.getPrecentOfNumbers(hours, actualHoursforWorker),
-            teamLeader: worker.TeamLeaderName,
-            hours: worker.Hours
-            // presence: this.baseService.toShortNumber(presenceHoursForWorker),
-            // percent: worker.workerHours.length ? this.baseService.toPercent(presenceHoursForWorker / worker.workerHours[0].numHours) : '-'
-          }
-        };
-        departmentNode2.children.push(workerNode);
-      }
-    })
-    root.children.push(departmentNode2);
-    return <TreeNode>(root);
-  }
-*/  
-
 
 
