ca21a9f2667384fd4767818971cc0d160b97f6a6
NechameShraiber <Nechama.Shraiber@seldatinc.com>
2018-11-29 09:51:52 +0200

    1.0

--- winForms/TaskManagment/TaskManagment/Forms/TeamLeader/TeamLeaderWorkers.cs
+++ winForms/TaskManagment/TaskManagment/Forms/TeamLeader/TeamLeaderWorkers.cs
@@ -74,9 +74,30 @@ namespace TaskManagment.Forms
         /// <summary>
         /// change allocated Hours
         /// </summary>
-       
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
         private void btn_change_Click(object sender, EventArgs e)
         {
+            ShowDialog("gfsdg", "gfsdgsdg");
             lblMessage.Text = "";
             int index;
             try {
