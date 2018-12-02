5e6d61995b550e4c309d6c031e4d83bb2e37d19e
NechamaShraiber <Nechama.Shraiber@seldatinc.com>
2018-12-02 10:25:28 +0200

    r2

--- PHP/UIL/index.php
+++ PHP/UIL/index.php
@@ -5,12 +5,19 @@ header("Access-Control-Request-Headers: *");
 $connection;
 $funcation = $_GET['funcation'];
 switch ($funcation) {
+    //manager
     case "getAllWorkers":
         getAllWorkers();
         break;
     case "getAllManagers":
         getAllManagers();
         break;
+    case "updateWorker":
+            updateWorker();
+        break;
+    
+    
+    ///
     case "getAllTeamLeaders":
 
         break;
@@ -39,6 +46,7 @@ function getAllWorkers() {
     header('Content-type: application/json');
     die(json_encode($CleintUser));
 }
+
 function getAllManagers() {
     global $connection;
     connection();
@@ -55,7 +63,7 @@ function getAllManagers() {
     header('Content-type: application/json');
     die(json_encode($CleintUser));
 }
-function editWorker() {
+function updateWorker() {
     global $connection;
     connection();
     header('Content-type: application/json');
@@ -74,7 +82,7 @@ function editWorker() {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     }
-       $query = "UPDATE truth_time_ct.users SET userName=" .$_POST['user']['UserName'].",password='{user.Password}',idStatus={user.IdStatus},emailUser='{user.EmailUser}',sumHours={user.SumHours},idTeamLeader={user.IdTeamLeader},isActive={user.IsActive} WHERE idUser={user.IdUser}";
+       $query = "UPDATE task_managment.workers SET name=" .$_POST['user']['Name'].",user_name='{user.UserName}',email={user.EMail},job='{user.JobId}',manager={user.ManagerId}WHERE worker_id={user.Id}";
     $resultObj = $connection->query($query);
     $users = array();
 $CleintUser = array();
